6faf6ab8d207fa30ac6e493d8fe7493ab96093a0
nstppbl <1nstppbl@gmail.com>
2022-09-03 15:22:14 +0500

    second commit

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int i, j;
+        for (i = 1; i <= sideLength; i++)
+        {
+            for (j = 1; j <= sideLength; j++)
+            {
+                if( i==j || i - 1 == sideLength - j )
+                    System.out.print("8");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
-
+        int i, j;
+        for (i = 1; i <= length; i++)
+        {
+            for (j = 1; j <= height; j++)
+            {
+                if (i == 1 || i == length ||
+                        j == 1 || j == height)
+                    System.out.print("8");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
